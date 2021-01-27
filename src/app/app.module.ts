b4c482664ec6298407eac66619b712f41e4b4c32
Pablo <pablosanchez169@gmail.com>
2021-01-27 18:23:29 +0100

    Incluir formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
+  
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Components/correo/correo.component';
 import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
-import { FormCorreoComponent } from './Components/form-correo/form-correo.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CorreoComponent,
     ListaCorreosComponent,
-    FormCorreoComponent
+    NuevoCorreoComponent
   ],
   imports: [
     BrowserModule,
